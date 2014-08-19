0775f0d7a541b3372ec2150f765881a752eb1afc
Remco Tolsma <info@remcotolsma.nl>
2014-08-19 15:45:36 +0200

    Added an payment method parameter.

--- src/Gateway.php
+++ src/Gateway.php
@@ -257,7 +257,7 @@ abstract class Pronamic_WP_Pay_Gateway {
 	 *
 	 * @param Pronamic_Pay_PaymentDataInterface $data
 	 */
-	public function start( Pronamic_Pay_PaymentDataInterface $data, Pronamic_Pay_Payment $payment ) {
+	public function start( Pronamic_Pay_PaymentDataInterface $data, Pronamic_Pay_Payment $payment, $payment_method = null ) {
 
 	}
 
