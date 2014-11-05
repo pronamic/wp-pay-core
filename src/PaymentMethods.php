4831876884b7eb6f2efe277b0106b3ced9ac1fae
Remco Tolsma <info@remcotolsma.nl>
2014-11-05 11:48:17 +0100

    Added an class with constant for payment methods.

---
+++ src/PaymentMethods.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Title: WordPress pay payment methods
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_PaymentMethods {
+	/**
+	 * iDEAL
+	 *
+	 * @var string
+	 */
+	const IDEAL = 'ideal';
+
+	/**
+	 * Bancontact/Mister Cash
+	 *
+	 * @var string
+	 */
+	const MISTER_CASH = 'mister_cash';
+
+	/**
+	 * MiniTix
+	 *
+	 * @var string
+	 */
+	const MINITIX = 'minitix';
+}
