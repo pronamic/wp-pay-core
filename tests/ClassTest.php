751a46b31411e01b9b78aff18ce1f42dfbb0c238
Remco Tolsma <info@remcotolsma.nl>
2015-02-27 15:40:41 +0100

    Added some tests.

---
+++ tests/ClassTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Title: WordPress pay class test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.1.0
+ * @since 1.1.0
+ */
+class Pronamic_WP_Pay_ClassTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @dataProvider status_matrix_provider
+	 */
+	public function test_method_exists( $class, $method, $expected ) {
+		$exists = Pronamic_WP_Pay_Class::method_exists( $class, $method );
+
+		$this->assertEquals( $expected, $exists );
+	}
+
+	public function status_matrix_provider() {
+		return array(
+			array( 'Pronamic_WP_Pay_Class', 'method_exists', true ),
+			array( 'Pronamic_WP_Pay_Server', 'get', true ),
+			array( 'ClassDoesNotExist', 'method_does_not_exist', false ),
+			array( '', '', false ),
+			array( null, null, false ),
+		);
+	}
+}
---
+++ tests/ServerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Title: WordPress pay server test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.1.0
+ * @since 1.1.0
+ */
+class Pronamic_WP_Pay_ServerTest extends PHPUnit_Framework_TestCase {
+	public function test_server_get() {
+		$value = Pronamic_WP_Pay_Server::get( 'REQUEST_METHOD', FILTER_SANITIZE_STRING );
+
+		$expected = null;
+
+		$this->assertEquals( $expected, $value );
+	}
+}
