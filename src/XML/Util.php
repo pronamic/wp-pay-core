a88922c7e2b7cfe3b6729c0ae986fe457e1c095b
Remco Tolsma <info@remcotolsma.nl>
2015-04-29 16:40:42 +0200

    Added an XML utility class.

---
+++ src/XML/Util.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Title: XML utility class
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @since 1.2.1
+ * @version 1.2.1
+ */
+class Pronamic_WP_Pay_XML_Util {
+	/**
+	 * Create and add an element with the specified name and value to the specified parent
+	 *
+	 * @param DOMDocument $document
+	 * @param DOMNode $parent
+	 * @param string $name
+	 * @param string $value
+	 */
+	public static function add_element( DOMDocument $document, DOMNode $parent, $name, $value = null ) {
+		$element = $document->createElement( $name );
+
+		if ( $value !== null ) {
+			$element->appendChild( new DOMText( $value ) );
+		}
+
+		$parent->appendChild( $element );
+
+		return $element;
+	}
+
+	/**
+	 * Add the specified elements to the parent node
+	 *
+	 * @param DOMDocument $document
+	 * @param DOMNode $parent
+	 * @param array $elements
+	 */
+	public static function add_elements( DOMDocument $document, DOMNode $parent, array $elements = array() ) {
+		foreach ( $elements as $name => $value ) {
+			$element = $document->createElement( $name );
+
+			if ( $value !== null ) {
+				$element->appendChild( new DOMText( $value ) );
+			}
+
+			$parent->appendChild( $element );
+		}
+	}
+}
