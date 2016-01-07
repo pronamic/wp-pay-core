30eb1b9fe920fda59be452160d7c08cdc00d8b8a
Remco Tolsma <info@remcotolsma.nl>
2016-01-07 16:58:56 +0100

    Also try the parent class to fix issue with extended config.

--- src/GatewayFactory.php
+++ src/GatewayFactory.php
@@ -21,6 +21,10 @@ class Pronamic_WP_Pay_GatewayFactory {
 		if ( isset( $config ) ) {
 			$config_class = get_class( $config );
 
+			if ( ! isset( self::$gateways[ $config_class ] ) ) {
+				$config_class = get_parent_class( $config );
+			}
+
 			if ( isset( self::$gateways[ $config_class ] ) ) {
 				$gateway_class = self::$gateways[ $config_class ];
 
