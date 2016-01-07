ffc97bbb615f8122533aceeebf299bac20cabbd6
Remco Tolsma <info@remcotolsma.nl>
2016-01-07 09:39:34 +0100

    Added support for PHP 5.2.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,10 @@ if ( ! $_tests_dir ) {
 
 require_once $_tests_dir . '/includes/functions.php';
 
-require_once __DIR__ . '/../vendor/autoload.php';
+if ( version_compare( PHP_VERSION, '5.3', '>=' ) ) {
+	require_once __DIR__ . '/../vendor/autoload.php';
+} else {
+	require_once __DIR__ . '/../vendor/autoload_52.php';
+}
 
 require $_tests_dir . '/includes/bootstrap.php';
