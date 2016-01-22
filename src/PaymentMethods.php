5d5ee891c247be8c634b2eb3f3a23799894874da
Remco Tolsma <info@remcotolsma.nl>
2016-01-22 16:28:24 +0100

    Getting ready for version 1.3.1.

--- src/PaymentMethods.php
+++ src/PaymentMethods.php
@@ -6,7 +6,7 @@
  * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.3.0
+ * @version 1.3.1
  * @since 1.0.1
  */
 class Pronamic_WP_Pay_PaymentMethods {
--- src/Util.php
+++ src/Util.php
@@ -6,7 +6,7 @@
  * Copyright: Copyright (c) 2005 - 2016
  * Company: Pronamic
  * @author Remco Tolsma
- * @version 1.3.0
+ * @version 1.3.1
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Util {
@@ -110,6 +110,7 @@ class Pronamic_WP_Pay_Util {
 	/**
 	 * String to amount (user input string)
 	 *
+	 * @version 1.3.1
 	 * @since 1.3.0
 	 * @param string $amount
 	 * @return float
