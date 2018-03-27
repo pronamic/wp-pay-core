db75e33bbea057acb02e4e57131d6ecbd5cfbb1c
Reüel van der Steege <reuel@pronamic.nl>
2018-03-27 09:01:24 +0200

    Create gateway instance only once in gateway factory.

--- src/GatewayFactory.php
+++ src/GatewayFactory.php
@@ -13,15 +13,39 @@ namespace Pronamic\WordPress\Pay\Core;
  * @since 1.0.0
  */
 class GatewayFactory {
+	/**
+	 * Created gateways.
+	 *
+	 * @var array
+	 */
+	static protected $gateways = array();
+
+	/**
+	 * Create gateway.
+	 *
+	 * @param GatewayConfig|null $config Gateway configuration.
+	 *
+	 * @return Gateway|null
+	 */
 	public static function create( GatewayConfig $config = null ) {
 		$gateway = null;
 
-		if ( isset( $config ) ) {
-			$gateway_class = $config->get_gateway_class();
+		if ( null === $config ) {
+			return $gateway;
+		}
 
-			if ( class_exists( $gateway_class, true ) ) {
-				$gateway = new $gateway_class( $config );
-			}
+		// Return existing gateway for configuration if it exists.
+		if ( isset( self::$gateways[ $config->id ] ) ) {
+			return self::$gateways[ $config->id ];
+		}
+
+		// Create new gateway from gateway class.
+		$gateway_class = $config->get_gateway_class();
+
+		if ( class_exists( $gateway_class, true ) ) {
+			$gateway = new $gateway_class( $config );
+
+			self::$gateways[ $config->id ] = $gateway;
 		}
 
 		return $gateway;
