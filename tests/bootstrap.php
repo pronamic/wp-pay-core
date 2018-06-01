124bd5fad46d9ac3e20e699a215b5accb0b5a515
Reüel van der Steege <reuel@pronamic.nl>
2018-06-01 14:11:37 +0200

    Update tests bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,8 +12,6 @@ putenv( 'WP_PHPUNIT__TESTS_CONFIG=' . __DIR__ . '/wp-config.php' );
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define( 'WP_PHPUNIT__TESTS_CONFIG', dirname( __FILE__ ) . '/wp-config.php' );
-
 require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
 require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
