24991ccb8d87e147825a6685672326e2aeee4a05
Remco Tolsma <info@remcotolsma.nl>
2018-06-21 13:48:24 +0200

    Updated version number.

--- src/Admin/AdminModule.php
+++ src/Admin/AdminModule.php
@@ -434,7 +434,7 @@ class AdminModule {
 				'tippy.js',
 				plugins_url( 'assets/tippy.js/tippy.all' . $min . '.js', $this->plugin->get_file() ),
 				array(),
-				'2.5.2',
+				'2.5.3',
 				true
 			);
 
