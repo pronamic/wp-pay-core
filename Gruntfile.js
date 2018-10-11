13db90ef53816780a6ef64ff6476caa9ec55c448
Remco Tolsma <info@remcotolsma.nl>
2018-10-11 20:03:01 +0200

    Simplified some commands.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,59 +11,61 @@ module.exports = function( grunt ) {
 	require( 'load-grunt-tasks' )( grunt );
 
 	// Project configuration.
-	grunt.initConfig( {
-		// Package.
-		pkg: grunt.file.readJSON( 'package.json' ),
+	grunt.initConfig(
+		{
+				// Package.
+			pkg: grunt.file.readJSON( 'package.json' ),
 
-		// JSHint.
-		jshint: {
-			all: [ 'Gruntfile.js', 'composer.json', 'package.json' ]
-		},
-
-		// PHP Code Sniffer.
-		phpcs: {
-			application: {
-				src: [
-					'**/*.php',
-					'!node_modules/**',
-					'!vendor/**'
-				],
+				// JSHint.
+			jshint: {
+				all: [ 'Gruntfile.js', 'composer.json', 'package.json' ]
 			},
-			options: {
-				bin: 'vendor/bin/phpcs',
-				standard: 'phpcs.xml.dist',
-				showSniffCodes: true
-			}
-		},
 
-		// PHPLint.
-		phplint: {
-			all: [ 'src/**/*.php' ]
-		},
-
-		// PHP Mess Detector.
-		phpmd: {
-			application: {
-				dir: 'src'
+				// PHP Code Sniffer.
+			phpcs: {
+				application: {
+					src: [
+						'**/*.php',
+						'!node_modules/**',
+						'!vendor/**'
+					],
+				},
+				options: {
+					bin: 'vendor/bin/phpcs',
+					standard: 'phpcs.xml.dist',
+					showSniffCodes: true
+				}
 			},
-			options: {
-				bin: 'vendor/bin/phpmd',
-				exclude: 'node_modules',
-				reportFormat: 'text',
-				rulesets: 'phpmd.ruleset.xml'
-			}
-		},
 
-		// PHPUnit.
-		phpunit: {
-			options: {
-				bin: 'vendor/bin/phpunit'
+				// PHPLint.
+			phplint: {
+				all: [ 'src/**/*.php' ]
 			},
-			classes: {
 
+				// PHP Mess Detector.
+			phpmd: {
+				application: {
+					dir: 'src'
+				},
+				options: {
+					bin: 'vendor/bin/phpmd',
+					exclude: 'node_modules',
+					reportFormat: 'text',
+					rulesets: 'phpmd.ruleset.xml'
+				}
+			},
+
+				// PHPUnit.
+			phpunit: {
+				options: {
+					bin: 'vendor/bin/phpunit'
+				},
+				classes: {
+
+				}
 			}
 		}
-	} );
+	);
 
 	// Default task(s).
 	grunt.registerTask( 'default', [ 'jshint', 'phplint', 'phpmd', 'phpcs', 'phpunit' ] );
