aa1d46add7f0d005822ac9fa975ab14e84d2c1b6
Remco Tolsma <info@remcotolsma.nl>
2018-11-07 15:42:46 +0100

    Added legacy properties.

--- src/Payments/LegacyPayment.php
+++ src/Payments/LegacyPayment.php
@@ -24,6 +24,18 @@ use Pronamic\WordPress\Pay\Customer;
  * @author  Remco Tolsma
  * @version 2.0.8
  * @since   2.0.8
+ *
+ * @property string $language
+ * @property string $locale
+ * @property string $email
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $telephone_number
+ * @property string $country
+ * @property string $zip
+ * @property string $city
+ * @property string $address
+ * @property string $user_id
  */
 abstract class LegacyPayment {
 	/**
