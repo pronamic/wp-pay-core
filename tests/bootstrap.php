aa53628f7d70df187f63e8534b1e35ca1a44c310
Reüel van der Steege <reuel@pronamic.nl>
2019-02-09 03:15:14 +0100

    Update libraries, coding standards, tests and Travis config.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,13 +5,23 @@
  * @author    Pronamic <info@pronamic.eu>
  * @copyright 2005-2019 Pronamic
  * @license   GPL-3.0-or-later
- * @package   Pronamic\WordPress\Pay\Extensions\MemberPress
+ * @package   Pronamic\WordPress\Pay\Core
  */
-
-putenv( 'WP_PHPUNIT__TESTS_CONFIG=tests/wp-config.php' );
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
+/**
+ * Manually load plugin.
+ */
+function _manually_load_plugin() {
+	global $pronamic_ideal;
+
+	$pronamic_ideal = pronamic_pay_plugin();
+}
+
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Bootstrap.
 require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
