0f336f4c395c1cc9bd5630a0f66122d66cefa0fd
Reüel van der Steege <reuel@pronamic.nl>
2019-03-25 14:38:00 +0100

    Change add/remove admin notice to static methods.

--- src/Admin/AdminNotices.php
+++ src/Admin/AdminNotices.php
@@ -106,7 +106,7 @@ class AdminNotices {
 	 *
 	 * @param string $name Name.
 	 */
-	public function add_notice( $name ) {
+	public static function add_notice( $name ) {
 		$notices = get_option( 'pronamic_pay_admin_notices', array() );
 
 		$notices = is_array( $notices ) ? $notices : array();
@@ -121,7 +121,7 @@ class AdminNotices {
 	 *
 	 * @param string $name Name.
 	 */
-	public function remove_notice( $name ) {
+	public static function remove_notice( $name ) {
 		$notices = get_option( 'pronamic_pay_admin_notices', array() );
 
 		$notices = is_array( $notices ) ? $notices : array();
