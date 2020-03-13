74f0693e09d30753d5812bff1da6288d0dc16d8d
Remco Tolsma <info@remcotolsma.nl>
2020-03-13 10:31:26 +0100

    Added abstract gateway integration test.

---
+++ tests/src/AbstractGatewayIntegrationTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Abstract gateway integration test
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2020 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Payments
+ */
+
+namespace Pronamic\WordPress\Pay;
+
+/**
+ * Abstract gateway integration test
+ *
+ * @author  Remco Tolsma
+ * @version 2.2.6
+ * @since   1.0.0
+ */
+class AbstractGatewayIntegrationTest extends \PHPUnit\Framework\TestCase {
+	/**
+	 * Test gateway integration.
+	 */
+	public function test_gateway_integration() {
+		$mock = $this->getMockForAbstractClass( AbstractPluginIntegration::class );
+
+		$this->assertNull( $mock->get_version_option_name() );
+		$this->assertNull( $mock->get_db_version_option_name() );
+
+		$mock->set_version( '1.0.0' );
+		$mock->set_version_option_name( 'pronamic_pay_restrictcontentpro_version' );
+		$mock->set_db_version_option_name( 'pronamic_pay_restrictcontentpro_db_version' );
+
+		$this->assertEquals( '1.0.0', $mock->get_version() );
+		$this->assertEquals( 'pronamic_pay_restrictcontentpro_version', $mock->get_version_option_name() );
+		$this->assertEquals( 'pronamic_pay_restrictcontentpro_db_version', $mock->get_db_version_option_name() );
+
+		$this->assertEquals( '', $mock->get_version_option() );
+		$this->assertEquals( '', $mock->get_db_version_option() );
+
+		$mock->update_version_option();
+
+		$this->assertEquals( '1.0.0', $mock->get_version_option() );
+	}
+}
