7b84b331c976fd1713b478a48d8fbf486760b7e0
Remco Tolsma <info@remcotolsma.nl>
2020-08-05 20:48:37 +0200

    Added ID trait.

---
+++ src/Core/IdTrait.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Id Trait
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2020 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Privacy
+ */
+
+namespace Pronamic\WordPress\Pay\Core;
+
+/**
+ * Id Trait
+ *
+ * @author  Remco Tolsma
+ * @version unreleased
+ * @since   unreleased
+ * @link    https://github.com/search?q=%22trait+IdTrait%22+language%3APHP&type=Code
+ */
+trait IdTrait {
+	/**
+	 * ID.
+	 *
+	 * @var string|null
+	 */
+	private $id;
+
+	/**
+	 * Get the ID.
+	 *
+	 * @return int|null
+	 */
+	public function get_id() {
+		return $this->id;
+	}
+
+	/**
+	 * Set the ID.
+	 *
+	 * @param int $id Unique ID.
+	 * @return void
+	 */
+	public function set_id( $id ) {
+		$this->id = $id;
+	}
+}
