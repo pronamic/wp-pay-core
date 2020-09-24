3d30caf4e35b3f1f15b526fc1ccae84c19b3d624
Reüel van der Steege <reuel@pronamic.nl>
2020-09-24 16:32:33 +0200

    Add payment lines and customer to subscription builder.

--- src/Subscriptions/SubscriptionBuilder.php
+++ src/Subscriptions/SubscriptionBuilder.php
@@ -10,6 +10,9 @@
 
 namespace Pronamic\WordPress\Pay\Subscriptions;
 
+use Pronamic\WordPress\Pay\Customer;
+use Pronamic\WordPress\Pay\Payments\PaymentLines;
+
 /**
  * Subscription Builder
  *
@@ -18,6 +21,20 @@ namespace Pronamic\WordPress\Pay\Subscriptions;
  * @since   unreleased
  */
 class SubscriptionBuilder {
+	/**
+	 * Customer.
+	 *
+	 * @var Customer
+	 */
+	private $customer;
+
+	/**
+	 * Payment Lines.
+	 *
+	 * @var PaymentLines|null
+	 */
+	private $lines;
+
 	/**
 	 * Phases.
 	 *
@@ -30,6 +47,30 @@ class SubscriptionBuilder {
 	 */
 	public function __construct() {
 		$this->phases = array();
+	}
+
+	/**
+	 * With customer.
+	 *
+	 * @param Customer $customer Customer.
+	 * @return $this
+	 */
+	public function with_customer( Customer $customer ) {
+		$this->customer = $customer;
+
+		return $this;
+	}
+
+	/**
+	 * With lines.
+	 *
+	 * @param PaymentLines $lines Payment lines.
+	 * @return $this
+	 */
+	public function with_lines( PaymentLines $lines ) {
+		$this->lines = $lines;
+
+		return $this;
 	}
 
 	/**
@@ -53,6 +94,13 @@ class SubscriptionBuilder {
 	public function create() {
 		$subscription = new Subscription();
 
+		// Customer.
+		$subscription->set_customer( $this->customer );
+
+		// Lines.
+		$subscription->set_lines( $this->lines );
+
+		// Phases.
 		foreach ( $this->phases as $phase ) {
 			$subscription->add_phase( $phase );
 		}
