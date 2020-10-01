c31795ca9def574b454947715f5eff0af5eb8068
Remco Tolsma <info@remcotolsma.nl>
2020-10-01 16:26:28 +0200

    Added tests for multiply function, requires some work, exceptions?

--- tests/src/Subscriptions/SubscriptionIntervalTest.php
+++ tests/src/Subscriptions/SubscriptionIntervalTest.php
@@ -26,4 +26,48 @@ class SubscriptionIntervalTest extends \WP_UnitTestCase {
 		$this->assertEquals( 'P2Y6M', \strval( $interval ) );
 		$this->assertEquals( '"P2Y6M"', \wp_json_encode( $interval ) );
 	}
+
+	/**
+	 * Test multiply.
+	 */
+	public function test_multiply() {
+		$interval = new SubscriptionInterval( 'P2Y6M' );
+
+		$result = $interval->multiply( 2 );
+
+		$this->assertEquals( 'P4Y12M', \strval( $result ) );
+	}
+
+	/**
+	 * Test multiply by one.
+	 */
+	public function test_multiply_by_one() {
+		$interval = new SubscriptionInterval( 'P2Y6M' );
+
+		$result = $interval->multiply( 1 );
+
+		$this->assertEquals( 'P2Y6M', \strval( $result ) );
+	}
+
+	/**
+	 * Test multiply by zero.
+	 */
+	public function test_multiply_by_zero() {
+		$interval = new SubscriptionInterval( 'P2Y6M' );
+
+		$result = $interval->multiply( 0 );
+
+		$this->assertEquals( 'P0Y0M', \strval( $result ) );
+	}
+
+	/**
+	 * Test negative multiplier.
+	 */
+	public function test_negative_multiplier() {
+		$interval = new SubscriptionInterval( 'P2Y6M' );
+
+		$result = $interval->multiply( -2 );
+
+		$this->assertEquals( 'P4Y12M', \strval( $result ) );
+	}
 }
