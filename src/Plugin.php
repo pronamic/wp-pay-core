2dabc42aa041ebd7b59677c5003657026ede9c27
Remco Tolsma <info@remcotolsma.nl>
2020-10-08 15:37:52 +0200

    Shorter notation.

--- src/Plugin.php
+++ src/Plugin.php
@@ -692,9 +692,8 @@ class Plugin {
 
 		if ( null !== $payment_method ) {
 			$config_ids = PaymentMethods::get_config_ids( $payment_method );
-			$config_ids = empty( $config_ids ) ? array( 0 ) : $config_ids;
 
-			$args['post__in'] = $config_ids;
+			$args['post__in'] = empty( $config_ids ) ? array( 0 ) : $config_ids;
 		}
 
 		$query = new WP_Query( $args );
