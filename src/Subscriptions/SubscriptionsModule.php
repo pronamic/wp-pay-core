293d3fd5157514a238dde976923d583de13f5969
Reüel van der Steege <reuel@pronamic.nl>
2020-10-29 14:32:34 +0100

    Update setting periods created on subscription creation to account for multiple phases.

--- src/Subscriptions/SubscriptionsModule.php
+++ src/Subscriptions/SubscriptionsModule.php
@@ -560,9 +560,11 @@ class SubscriptionsModule {
 		}
 
 		// Set periods created in current phase.
+		$phases = $subscription->get_phases();
+
 		$current_phase = $subscription->get_current_phase();
 
-		if ( null !== $current_phase && 0 === $current_phase->get_periods_created() ) {
+		if ( 1 === count( $phases ) && null !== $current_phase && 0 === $current_phase->get_periods_created() ) {
 			$current_phase->set_periods_created( 1 );
 		}
 
