6fdcd10b0fd4af469e5bcfab6148024ff2cd7ce1
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2021-08-04 16:00:48 +0200

    Fix "Code after the RETURN statement on line 23 cannot be".

--- views/meta-box-payment-lines.php
+++ views/meta-box-payment-lines.php
@@ -20,204 +20,248 @@ if ( empty( $lines ) ) : ?>
 		<?php \esc_html_e( 'No payment lines found.', 'pronamic_ideal' ); ?>
 	</p>
 
-	<?php return; ?>
+<?php else : ?>
 
-<?php endif; ?>
+	<div class="pronamic-pay-table-responsive">
+		<table class="pronamic-pay-table widefat">
+			<thead>
+				<tr>
+					<th scope="col"><?php \esc_html_e( 'ID', 'pronamic_ideal' ); ?></th>
+					<th scope="col"><?php \esc_html_e( 'SKU', 'pronamic_ideal' ); ?></th>
+					<th scope="col"><?php \esc_html_e( 'Image', 'pronamic_ideal' ); ?></th>
+					<th scope="col"><?php \esc_html_e( 'Name', 'pronamic_ideal' ); ?></th>
+					<th scope="col">
+						<?php
 
-<div class="pronamic-pay-table-responsive">
-	<table class="pronamic-pay-table widefat">
-		<thead>
-			<tr>
-				<th scope="col"><?php \esc_html_e( 'ID', 'pronamic_ideal' ); ?></th>
-				<th scope="col"><?php \esc_html_e( 'SKU', 'pronamic_ideal' ); ?></th>
-				<th scope="col"><?php \esc_html_e( 'Image', 'pronamic_ideal' ); ?></th>
-				<th scope="col"><?php \esc_html_e( 'Name', 'pronamic_ideal' ); ?></th>
-				<th scope="col">
-					<?php
+						\printf(
+							'<span class="pronamic-pay-tip" title="%s">%s</span>',
+							\esc_attr__( 'Unit price with discount including tax.', 'pronamic_ideal' ),
+							\esc_html__( 'Unit Price', 'pronamic_ideal' )
+						);
 
-					\printf(
-						'<span class="pronamic-pay-tip" title="%s">%s</span>',
-						\esc_attr__( 'Unit price with discount including tax.', 'pronamic_ideal' ),
-						\esc_html__( 'Unit Price', 'pronamic_ideal' )
-					);
+						?>
+					</th>
+					<th scope="col"><?php \esc_html_e( 'Quantity', 'pronamic_ideal' ); ?></th>
+					<th scope="col">
+						<?php
 
-					?>
-				</th>
-				<th scope="col"><?php \esc_html_e( 'Quantity', 'pronamic_ideal' ); ?></th>
-				<th scope="col">
-					<?php
+						\printf(
+							'<span class="pronamic-pay-tip" title="%s">%s</span>',
+							\esc_attr__( 'Total discount.', 'pronamic_ideal' ),
+							\esc_html__( 'Discount', 'pronamic_ideal' )
+						);
 
-					\printf(
-						'<span class="pronamic-pay-tip" title="%s">%s</span>',
-						\esc_attr__( 'Total discount.', 'pronamic_ideal' ),
-						\esc_html__( 'Discount', 'pronamic_ideal' )
-					);
+						?>
+					</th>
+					<th scope="col">
+						<?php
 
-					?>
-				</th>
-				<th scope="col">
-					<?php
+						\printf(
+							'<span class="pronamic-pay-tip" title="%s">%s</span>',
+							\esc_attr__( 'Total amount with discount including tax.', 'pronamic_ideal' ),
+							\esc_html__( 'Total Amount', 'pronamic_ideal' )
+						);
 
-					\printf(
-						'<span class="pronamic-pay-tip" title="%s">%s</span>',
-						\esc_attr__( 'Total amount with discount including tax.', 'pronamic_ideal' ),
-						\esc_html__( 'Total Amount', 'pronamic_ideal' )
-					);
+						?>
+					</th>
+					<th scope="col"><?php \esc_html_e( 'Total Tax', 'pronamic_ideal' ); ?></th>
+				</tr>
+			</thead>
 
-					?>
-				</th>
-				<th scope="col"><?php \esc_html_e( 'Total Tax', 'pronamic_ideal' ); ?></th>
-			</tr>
-		</thead>
+			<tfoot>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>
+						<?php
 
-		<tfoot>
-			<tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td>
-					<?php
+						$quantities = \array_map(
+							function( PaymentLine $line ) {
+									return $line->get_quantity();
+							},
+							$lines->get_array()
+						);
 
-					$quantities = \array_map(
-						function( PaymentLine $line ) {
-								return $line->get_quantity();
-						},
-						$lines->get_array()
-					);
+						echo \esc_html( \array_sum( $quantities ) );
 
-					echo \esc_html( \array_sum( $quantities ) );
+						?>
+					</td>
+					<td>
+						<?php
 
-					?>
-				</td>
-				<td>
-					<?php
+						$values = \array_map(
+							function( PaymentLine $line ) {
+								$discount_amount = $line->get_discount_amount();
 
-					$values = \array_map(
-						function( PaymentLine $line ) {
+								return ( null === $discount_amount ) ? null : $discount_amount->get_value();
+							},
+							$lines->get_array()
+						);
+
+						$discount_amount = new Money( \array_sum( $values ), $lines->get_amount()->get_currency() );
+
+						echo \esc_html( $discount_amount->format_i18n() );
+
+						?>
+					</td>
+					<td>
+						<?php echo \esc_html( $lines->get_amount()->format_i18n() ); ?>
+					</td>
+					<td>
+						<?php
+
+						$values = \array_map(
+							function( PaymentLine $line ) {
+								$total_amount = $line->get_total_amount();
+
+								if ( $total_amount instanceof TaxedMoney ) {
+									return $total_amount->get_tax_value();
+								}
+
+								return null;
+							},
+							$lines->get_array()
+						);
+
+						$tax_amount = new Money( \array_sum( $values ), $lines->get_amount()->get_currency() );
+
+						echo \esc_html( $tax_amount->format_i18n() );
+
+						?>
+					</td>
+				</tr>
+			</tfoot>
+
+			<tbody>
+
+				<?php foreach ( $lines as $line ) : ?>
+
+					<tr>
+						<td><?php echo \esc_html( $line->get_id() ); ?></td>
+						<td><?php echo \esc_html( $line->get_sku() ); ?></td>
+						<td>
+							<?php
+
+							$image_url = $line->get_image_url();
+
+							if ( ! empty( $image_url ) ) {
+								\printf(
+									'<img src="%s" alt="" width="50" height="50" />',
+									\esc_url( $image_url )
+								);
+							}
+
+							?>
+						</td>
+						<td>
+							<?php
+
+							$product_url = $line->get_product_url();
+
+							$description = $line->get_description();
+
+							if ( ! empty( $product_url ) ) {
+								// Product URL with or without description.
+								$line_title = $line->get_name();
+
+								$classes = array();
+
+								if ( ! empty( $description ) ) {
+									$line_title = $line->get_description();
+									$classes[]  = 'pronamic-pay-tip';
+								}
+
+								\printf(
+									'<a class="%1$s" href="%2$s" title="%3$s">%4$s<a/>',
+									\esc_attr( \implode( ' ', $classes ) ),
+									\esc_url( $line->get_product_url() ),
+									\esc_attr( $line_title ),
+									\esc_html( $line->get_name() )
+								);
+							} elseif ( ! empty( $description ) ) {
+								// Description without product URL.
+								\printf(
+									'<span class="pronamic-pay-tip" title="%1$s">%2$s</span>',
+									\esc_attr( $line->get_description() ),
+									\esc_html( $line->get_name() )
+								);
+							} else {
+								// No description and no product URL.
+								echo \esc_html( $line->get_name() );
+							}
+
+							?>
+						</td>
+						<td>
+							<?php
+
+							$unit_price = $line->get_unit_price();
+
+							if ( null !== $unit_price ) {
+								$tips = array(
+									\__( 'No tax information.', 'pronamic_ideal' ),
+								);
+
+								if ( $unit_price instanceof TaxedMoney ) {
+									$tips = array(
+										\sprintf(
+											/* translators: %s: price excluding tax */
+											\__( 'Exclusive tax: %s', 'pronamic_ideal' ),
+											$unit_price->get_excluding_tax()
+										),
+										\sprintf(
+											/* translators: %s: price including tax */
+											\__( 'Inclusive tax: %s', 'pronamic_ideal' ),
+											$unit_price->get_including_tax()
+										),
+									);
+								}
+
+								\printf(
+									'<span class="pronamic-pay-tip" title="%s">%s</span>',
+									\esc_attr( \implode( '<br />', $tips ) ),
+									\esc_html( $unit_price->format_i18n() )
+								);
+
+							}
+
+							?>
+						</td>
+						<td><?php echo \esc_html( $line->get_quantity() ); ?></td>
+						<td>
+							<?php
+
 							$discount_amount = $line->get_discount_amount();
 
-							return ( null === $discount_amount ) ? null : $discount_amount->get_value();
-						},
-						$lines->get_array()
-					);
-
-					$discount_amount = new Money( \array_sum( $values ), $lines->get_amount()->get_currency() );
-
-					echo \esc_html( $discount_amount->format_i18n() );
-
-					?>
-				</td>
-				<td>
-					<?php echo \esc_html( $lines->get_amount()->format_i18n() ); ?>
-				</td>
-				<td>
-					<?php
-
-					$values = \array_map(
-						function( PaymentLine $line ) {
-							$total_amount = $line->get_total_amount();
-
-							if ( $total_amount instanceof TaxedMoney ) {
-								return $total_amount->get_tax_value();
+							if ( null !== $discount_amount ) {
+								echo \esc_html( $discount_amount );
 							}
 
-							return null;
-						},
-						$lines->get_array()
-					);
+							?>
+						</td>
+						<td>
+							<?php
 
-					$tax_amount = new Money( \array_sum( $values ), $lines->get_amount()->get_currency() );
+							$line_total = $line->get_total_amount();
 
-					echo \esc_html( $tax_amount->format_i18n() );
-
-					?>
-				</td>
-			</tr>
-		</tfoot>
-
-		<tbody>
-
-			<?php foreach ( $lines as $line ) : ?>
-
-				<tr>
-					<td><?php echo \esc_html( $line->get_id() ); ?></td>
-					<td><?php echo \esc_html( $line->get_sku() ); ?></td>
-					<td>
-						<?php
-
-						$image_url = $line->get_image_url();
-
-						if ( ! empty( $image_url ) ) {
-							\printf(
-								'<img src="%s" alt="" width="50" height="50" />',
-								\esc_url( $image_url )
-							);
-						}
-
-						?>
-					</td>
-					<td>
-						<?php
-
-						$product_url = $line->get_product_url();
-
-						$description = $line->get_description();
-
-						if ( ! empty( $product_url ) ) {
-							// Product URL with or without description.
-							$line_title = $line->get_name();
-
-							$classes = array();
-
-							if ( ! empty( $description ) ) {
-								$line_title = $line->get_description();
-								$classes[]  = 'pronamic-pay-tip';
-							}
-
-							\printf(
-								'<a class="%1$s" href="%2$s" title="%3$s">%4$s<a/>',
-								\esc_attr( \implode( ' ', $classes ) ),
-								\esc_url( $line->get_product_url() ),
-								\esc_attr( $line_title ),
-								\esc_html( $line->get_name() )
-							);
-						} elseif ( ! empty( $description ) ) {
-							// Description without product URL.
-							\printf(
-								'<span class="pronamic-pay-tip" title="%1$s">%2$s</span>',
-								\esc_attr( $line->get_description() ),
-								\esc_html( $line->get_name() )
-							);
-						} else {
-							// No description and no product URL.
-							echo \esc_html( $line->get_name() );
-						}
-
-						?>
-					</td>
-					<td>
-						<?php
-
-						$unit_price = $line->get_unit_price();
-
-						if ( null !== $unit_price ) {
 							$tips = array(
 								\__( 'No tax information.', 'pronamic_ideal' ),
 							);
 
-							if ( $unit_price instanceof TaxedMoney ) {
+							if ( $line_total instanceof TaxedMoney ) {
 								$tips = array(
 									\sprintf(
 										/* translators: %s: price excluding tax */
 										\__( 'Exclusive tax: %s', 'pronamic_ideal' ),
-										$unit_price->get_excluding_tax()
+										$line->get_total_amount()->get_excluding_tax()
 									),
 									\sprintf(
 										/* translators: %s: price including tax */
 										\__( 'Inclusive tax: %s', 'pronamic_ideal' ),
-										$unit_price->get_including_tax()
+										$line->get_total_amount()->get_including_tax()
 									),
 								);
 							}
@@ -225,77 +269,33 @@ if ( empty( $lines ) ) : ?>
 							\printf(
 								'<span class="pronamic-pay-tip" title="%s">%s</span>',
 								\esc_attr( \implode( '<br />', $tips ) ),
-								\esc_html( $unit_price->format_i18n() )
+								\esc_html( $line_total->format_i18n() )
 							);
 
-						}
+							?>
+						</td>
+						<td>
+							<?php
 
-						?>
-					</td>
-					<td><?php echo \esc_html( $line->get_quantity() ); ?></td>
-					<td>
-						<?php
+							if ( $line_total instanceof TaxedMoney ) {
+								$tax_amount     = $line_total->get_tax_amount();
+								$tax_percentage = $line_total->get_tax_percentage();
 
-						$discount_amount = $line->get_discount_amount();
+								\printf(
+									'<span class="pronamic-pay-tip" title="%s">%s</span>',
+									\esc_attr( \number_format_i18n( $tax_percentage ) . '%' ),
+									\esc_html( $tax_amount->format_i18n() )
+								);
+							}
 
-						if ( null !== $discount_amount ) {
-							echo \esc_html( $discount_amount );
-						}
+							?>
+						</td>
+					</tr>
 
-						?>
-					</td>
-					<td>
-						<?php
+				<?php endforeach; ?>
 
-						$line_total = $line->get_total_amount();
+			</tbody>
+		</table>
+	</div>
 
-						$tips = array(
-							\__( 'No tax information.', 'pronamic_ideal' ),
-						);
-
-						if ( $line_total instanceof TaxedMoney ) {
-							$tips = array(
-								\sprintf(
-									/* translators: %s: price excluding tax */
-									\__( 'Exclusive tax: %s', 'pronamic_ideal' ),
-									$line->get_total_amount()->get_excluding_tax()
-								),
-								\sprintf(
-									/* translators: %s: price including tax */
-									\__( 'Inclusive tax: %s', 'pronamic_ideal' ),
-									$line->get_total_amount()->get_including_tax()
-								),
-							);
-						}
-
-						\printf(
-							'<span class="pronamic-pay-tip" title="%s">%s</span>',
-							\esc_attr( \implode( '<br />', $tips ) ),
-							\esc_html( $line_total->format_i18n() )
-						);
-
-						?>
-					</td>
-					<td>
-						<?php
-
-						if ( $line_total instanceof TaxedMoney ) {
-							$tax_amount     = $line_total->get_tax_amount();
-							$tax_percentage = $line_total->get_tax_percentage();
-
-							\printf(
-								'<span class="pronamic-pay-tip" title="%s">%s</span>',
-								\esc_attr( \number_format_i18n( $tax_percentage ) . '%' ),
-								\esc_html( $tax_amount->format_i18n() )
-							);
-						}
-
-						?>
-					</td>
-				</tr>
-
-			<?php endforeach; ?>
-
-		</tbody>
-	</table>
-</div>
+<?php endif; ?>
