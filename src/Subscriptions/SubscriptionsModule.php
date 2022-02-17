874e0ef92e94a6617b5924f0e16a9c745eda836a
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-02-17 13:30:58 +0100

    No longer need to check for gateway error, will use exceptions only.

--- src/Subscriptions/SubscriptionsModule.php
+++ src/Subscriptions/SubscriptionsModule.php
@@ -395,14 +395,6 @@ class SubscriptionsModule {
 				exit;
 			}
 
-			$error = $gateway->get_error();
-
-			if ( $error instanceof WP_Error ) {
-				Plugin::render_errors( $error );
-
-				exit;
-			}
-
 			// Redirect.
 			try {
 				$gateway->redirect( $payment );
