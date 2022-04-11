964c0275ccddb5b4777e5ba81a87262e7335e7e6
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-04-11 11:07:12 +0200

    Remove subscription phases trait.

--- src/Subscriptions/Subscription.php
+++ src/Subscriptions/Subscription.php
@@ -14,6 +14,7 @@ use DateInterval;
 use Pronamic\WordPress\DateTime\DateTime;
 use Pronamic\WordPress\DateTime\DateTimeInterface;
 use Pronamic\WordPress\DateTime\DateTimeImmutable;
+use Pronamic\WordPress\Money\Money;
 use Pronamic\WordPress\Pay\Payments\PaymentInfo;
 use Pronamic\WordPress\Pay\Payments\PaymentStatus;
 use Pronamic\WordPress\Pay\Payments\Payment;
@@ -27,8 +28,6 @@ use Pronamic\WordPress\Pay\Payments\PaymentInfoHelper;
  * @since   1.0.0
  */
 class Subscription extends PaymentInfo implements \JsonSerializable {
-	use SubscriptionPhasesTrait;
-
 	/**
 	 * The status of this subscription, for example 'Success'.
 	 *
@@ -47,6 +46,13 @@ class Subscription extends PaymentInfo implements \JsonSerializable {
 	 * @var DateTime
 	 */
 	private $activated_at;
+
+	/**
+	 * Phases.
+	 *
+	 * @var SubscriptionPhase[]
+	 */
+	private $phases = array();
 
 	/**
 	 * Construct and initialize subscription object.
@@ -662,5 +668,234 @@ class Subscription extends PaymentInfo implements \JsonSerializable {
 	 */
 	public function set_activated_at( DateTime $activated_at ) {
 		$this->activated_at = $activated_at;
+	}
+
+	/**
+	 * Get phases.
+	 *
+	 * @return array<int, SubscriptionPhase>
+	 */
+	public function get_phases() {
+		return $this->phases;
+	}
+
+	/**
+	 * Set phases.
+	 *
+	 * @param array<int, SubscriptionPhase> $phases Phases.
+	 * @return void
+	 */
+	public function set_phases( $phases ) {
+		$this->phases = $phases;
+	}
+
+	/**
+	 * Add the specified phase to this subscription.
+	 *
+	 * @param SubscriptionPhase $phase Phase.
+	 * @return void
+	 */
+	public function add_phase( SubscriptionPhase $phase ) {
+		$this->phases[] = $phase;
+
+		if ( null === $this->next_payment_date ) {
+			$this->next_payment_date = $phase->get_start_date();
+		}
+
+		$phase->set_sequence_number( \count( $this->phases ) );
+	}
+
+	/**
+	 * Create new phase for this subscription.
+	 *
+	 * @param \DateTimeInterface $start_date    Start date.
+	 * @param string             $interval_spec Interval specification.
+	 * @param Money              $amount        Amount.
+	 * @return SubscriptionPhase
+	 */
+	public function new_phase( $start_date, $interval_spec, $amount ) {
+		$interval = new SubscriptionInterval( $interval_spec );
+
+		$phase = new SubscriptionPhase( $this, $start_date, $interval, $amount );
+
+		$this->add_phase( $phase );
+
+		return $phase;
+	}
+
+	/**
+	 * Check if all the periods within the subscription phases are created.
+	 *
+	 * @return bool True if all created, false otherwise.
+	 */
+	public function all_periods_created() {
+		foreach ( $this->phases as $phase ) {
+			if ( ! $phase->all_periods_created() ) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Check if this subscription is infinite.
+	 *
+	 * @return bool True if infinite, false otherwise.
+	 */
+	public function is_infinite() {
+		foreach ( $this->phases as $phase ) {
+			if ( $phase->is_infinite() ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get current phase or null if all completed.
+	 *
+	 * @return SubscriptionPhase|null
+	 */
+	public function get_current_phase() {
+		foreach ( $this->phases as $phase ) {
+			if ( $phase->all_periods_created() ) {
+				continue;
+			}
+
+			if ( $phase->is_canceled() ) {
+				continue;
+			}
+
+			return $phase;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Get phase for display.
+	 *
+	 * @return SubscriptionPhase|null
+	 */
+	public function get_display_phase() {
+		// Get first uncompleted regular phase.
+		foreach ( $this->phases as $phase ) {
+			// Skip trial phases.
+			if ( $phase->is_trial() ) {
+				continue;
+			}
+
+			// Skip prorated phases.
+			if ( $phase->is_prorated() ) {
+				continue;
+			}
+
+			if ( ! $phase->all_periods_created() ) {
+				return $phase;
+			}
+		}
+
+		// Get first regular phase.
+		foreach ( $this->phases as $phase ) {
+			// Skip trial phases.
+			if ( $phase->is_trial() ) {
+				continue;
+			}
+
+			// Skip prorated phases.
+			if ( $phase->is_prorated() ) {
+				continue;
+			}
+
+			return $phase;
+		}
+
+		// Get first phase.
+		foreach ( $this->phases as $phase ) {
+			return $phase;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Check if subscription is in a trial period.
+	 *
+	 * @return bool True if current period definition is a trial, false otherwise.
+	 */
+	public function in_trial_period() {
+		$current_phase = $this->get_current_phase();
+
+		if ( null === $current_phase ) {
+			return false;
+		}
+
+		return $current_phase->is_trial();
+	}
+
+	/**
+	 * Get the next period.
+	 *
+	 * @return SubscriptionPeriod|null
+	 */
+	public function get_next_period() {
+		$current_phase = $this->get_current_phase();
+
+		if ( null === $current_phase ) {
+			return null;
+		}
+
+		return $current_phase->get_next_period();
+	}
+
+	/**
+	 * Next period.
+	 *
+	 * @return SubscriptionPeriod|null
+	 */
+	public function next_period() {
+		$current_phase = $this->get_current_phase();
+
+		if ( null === $current_phase ) {
+			return null;
+		}
+
+		return $current_phase->next_period();
+	}
+
+	/**
+	 * Get phase by sequence number.
+	 *
+	 * @param int $sequence_number Sequence number.
+	 * @return SubscriptionPhase|null
+	 */
+	public function get_phase_by_sequence_number( $sequence_number ) {
+		/**
+		 * PHP arrays are zero-based indexed, sequence number starts from 1.
+		 */
+		$key = $sequence_number - 1;
+
+		if ( array_key_exists( $key, $this->phases ) ) {
+			return $this->phases[ $key ];
+		}
+
+		return null;
+	}
+
+	/**
+	 * Get end date.
+	 *
+	 * @return DateTimeImmutable|null
+	 */
+	public function get_end_date() {
+		$end_phase = \end( $this->phases );
+
+		if ( false === $end_phase ) {
+			return null;
+		}
+
+		return $end_phase->get_end_date();
 	}
 }
--- tests/src/Subscriptions/SubscriptionTest.php
+++ tests/src/Subscriptions/SubscriptionTest.php
@@ -178,4 +178,97 @@ class SubscriptionTest extends TestCase {
 		$this->assertEquals( '2005-06-05', $period->get_end_date()->format( 'Y-m-d' ) );
 		$this->assertEquals( 89.95, $period->get_amount()->get_value() );
 	}
+	/**
+	 * Create new subscription.
+	 *
+	 * @return Subscription
+	 * @throws \Exception Throws exception on invalid date interval.
+	 */
+	private function new_subscription() {
+		$subscription = new Subscription();
+
+		return $subscription;
+	}
+
+	/**
+	 * New phase for subscription.
+	 *
+	 * @param Subscription $subscription Subscription.
+	 * @return SubscriptionPhase
+	 * @throws \Exception Throws exception on invalid date interval.
+	 */
+	private function new_phase( $subscription ) {
+		$phase = $subscription->new_phase( new \DateTimeImmutable(), 'P1W', new TaxedMoney( 50, 'EUR' ) );
+
+		return $phase;
+	}
+
+	/**
+	 * Test new period definition.
+	 */
+	public function test_new_period_definition() {
+		$subscription = $this->new_subscription();
+
+		$phase_1 = $this->new_phase( $subscription );
+
+		$this->assertInstanceOf( SubscriptionPhase::class, $phase_1 );
+
+		$phase_2 = $this->new_phase( $subscription );
+
+		$this->assertInstanceOf( SubscriptionPhase::class, $phase_2 );
+	}
+
+	/**
+	 * Test infinite.
+	 */
+	public function test_infinite() {
+		$subscription = $this->new_subscription();
+
+		$phase_1 = $this->new_phase( $subscription );
+		$phase_2 = $this->new_phase( $subscription );
+
+		$this->assertTrue( $subscription->is_infinite() );
+	}
+
+	/**
+	 * Test current period definition.
+	 */
+	public function test_current_period_definition() {
+		$subscription = $this->new_subscription();
+
+		$phase_1 = $this->new_phase( $subscription );
+		$phase_2 = $this->new_phase( $subscription );
+		$phase_3 = $this->new_phase( $subscription );
+
+		$phase_1->set_total_periods( 1 );
+		$phase_1->set_periods_created( 1 );
+
+		$current_phase = $subscription->get_current_phase();
+
+		$this->assertEquals( $phase_2, $current_phase );
+	}
+
+	/**
+	 * Test in trial period.
+	 */
+	public function test_in_trial_period() {
+		$subscription = $this->new_subscription();
+
+		$phase_1 = $this->new_phase( $subscription );
+		$phase_2 = $this->new_phase( $subscription );
+		$phase_3 = $this->new_phase( $subscription );
+
+		$current_phase = $subscription->get_current_phase();
+
+		$this->assertFalse( $subscription->in_trial_period() );
+
+		$phase_1->set_trial( true );
+
+		$this->assertTrue( $subscription->in_trial_period() );
+
+		$phase_1->set_total_periods( 1 );
+		$phase_1->set_periods_created( 1 );
+
+		$this->assertFalse( $subscription->in_trial_period() );
+	}
 }
