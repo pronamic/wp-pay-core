a28c5dcffae1d618bfb52752ab0fdb8eee21885d
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-04-14 13:44:20 +0200

    Update meta-box-gateway-config.php

--- views/meta-box-gateway-config.php
+++ views/meta-box-gateway-config.php
@@ -17,7 +17,7 @@ $integrations = iterator_to_array( $plugin->gateway_integrations );
 usort(
 	$integrations,
 	function( $integration_a, $integration_b ) {
-		return strcasecmp( $integration_a->get_name(), $integration_b->get_name() );
+		return strcasecmp( (string) $integration_a->get_name(), (string) $integration_b->get_name() );
 	}
 );
 
