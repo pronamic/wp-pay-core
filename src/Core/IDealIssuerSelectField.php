fb2744d97c97da6af38323ff55d24504f02a8685
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-08-17 10:21:16 +0200

    Create IDealIssuerSelectField.php

---
+++ src/Core/IDealIssuerSelectField.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Select field iDEAL issuer
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2022 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Core
+ */
+
+namespace Pronamic\WordPress\Pay\Core;
+
+/**
+ * Select field iDEAL issuer class
+ */
+class IDealIssuerSelectField extends SelectField {
+
+}
