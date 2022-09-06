17a5ff06d36e93dfd2cbdb53e9ee94138f6bdede
Reüel van der Steege <reuel@pronamic.nl>
2022-09-06 13:33:43 +0200

    Add text field.

---
+++ src/Fields/TextField.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Text field
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2022 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Core
+ */
+
+namespace Pronamic\WordPress\Pay\Fields;
+
+use Pronamic\WordPress\Html\Element;
+
+/**
+ * Text field class
+ */
+class TextField extends Field {
+	/**
+	 * Get HTML attributes.
+	 *
+	 * @return array<string, string>
+	 */
+	protected function get_html_attributes() : array {
+		$attributes = parent::get_html_attributes();
+
+		$attributes['type'] = 'text';
+		$attributes['id']   = $this->get_id();
+		$attributes['name'] = $this->get_id();
+
+		return $attributes;
+	}
+
+	/**
+	 * Render field.
+	 *
+	 * @return string
+	 */
+	public function render() : string {
+		$element = new Element( 'input', $this->get_html_attributes() );
+
+		return $element->render();
+	}
+
+	/**
+	 * Serialize to JSON.
+	 *
+	 * @return array
+	 */
+	public function jsonSerialize() : array {
+		$data = parent::jsonSerialize();
+
+		$data['type'] = 'input';
+
+		return $data;
+	}
+}
