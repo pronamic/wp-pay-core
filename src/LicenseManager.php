19088b1746901c973627bfbc1a081b961d8d6b8c
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-06-01 20:59:46 +0200

    Changes links in license notice.

--- src/LicenseManager.php
+++ src/LicenseManager.php
@@ -158,17 +158,17 @@ class LicenseManager {
 			if ( '' === $license ) {
 				$notice = sprintf(
 				/* translators: 1: Pronamic Pay settings page URL, 2: Pronamic.eu plugin page URL */
-					__( '<strong>Pronamic Pay</strong> — You have not entered a valid <a href="%1$s">support license key</a>, please <a href="%2$s" target="_blank">get your key at pronamic.eu</a>.', 'pronamic_ideal' ),
+					__( '<strong>Pronamic Pay</strong> — You have not entered a valid <a href="%1$s">support license key</a>, please <a href="%2$s" target="_blank">get your key at pronamicpay.com</a>.', 'pronamic_ideal' ),
 					add_query_arg( 'page', 'pronamic_pay_settings', get_admin_url( null, 'admin.php' ) ),
-					'https://www.pronamic.eu/plugins/pronamic-ideal/'
+					'https://www.pronamicpay.com/'
 				);
 			} else {
 				$notice = sprintf(
 				/* translators: 1: Pronamic Pay settings page URL, 2: Pronamic.eu plugin page URL, 3: Pronamic.eu account page URL */
-					__( '<strong>Pronamic Pay</strong> — You have not entered a valid <a href="%1$s">support license key</a>. Please <a href="%2$s" target="_blank">get your key at pronamic.eu</a> or login to <a href="%3$s" target="_blank">check your license status</a>.', 'pronamic_ideal' ),
+					__( '<strong>Pronamic Pay</strong> — You have not entered a valid <a href="%1$s">support license key</a>. Please <a href="%2$s" target="_blank">get your key at pronamicpay.com</a> or login to <a href="%3$s" target="_blank">check your license status</a>.', 'pronamic_ideal' ),
 					add_query_arg( 'page', 'pronamic_pay_settings', get_admin_url( null, 'admin.php' ) ),
-					'https://www.pronamic.eu/plugins/pronamic-ideal/',
-					'https://www.pronamic.eu/account/'
+					'https://www.pronamicpay.com/',
+					'https://www.pronamic.shop/'
 				);
 			}
 
