28257b5ca1d06380a4e808c9ed8bd4a09e9308f2
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-10-04 10:47:20 +0200

    Fix "The method parameter $args is never used" warning.

--- src/Subscriptions/SubscriptionsFollowUpPaymentsController.php
+++ src/Subscriptions/SubscriptionsFollowUpPaymentsController.php
@@ -51,7 +51,7 @@ class SubscriptionsFollowUpPaymentsController {
 
 		WP_CLI::add_command(
 			'pay subscription list',
-			function ( $args ) {
+			function () {
 				WP_CLI::debug( 'Query subscriptions that require follow-up payment.' );
 
 				$query = $this->get_subscriptions_wp_query_that_require_follow_up_payment();
