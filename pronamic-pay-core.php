c8aae03613573384298d7538d8f557f2c7a9cc1e
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-05-15 09:46:04 +0200

    Use SQLite integration for PHPUnit.

---
+++ pronamic-pay-core.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: Pronamic Pay Core
+ * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-core/
+ * Description: Core components for the WordPress payment processing library.
+ *
+ * Version: 4.16.0
+ * Requires at least: 5.9
+ * Requires PHP: 8.0
+ *
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ *
+ * Text Domain: pronamic-pay-core
+ * Domain Path: /languages/
+ *
+ * License: GPL-3.0-or-later
+ *
+ * GitHub URI: https://github.com/pronamic/wp-pay-core
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2023 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Autoload.
+ */
+require_once __DIR__ . '/vendor/autoload_packages.php';
+
+/**
+ * Bootstrap.
+ */
+\Pronamic\WordPress\Pay\Plugin::instance(
+	[
+		'file'             => __FILE__,
+		'action_scheduler' => __DIR__ . '/vendor/woocommerce/action-scheduler/action-scheduler.php',
+	]
+);
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,14 +9,11 @@
  */
 
 /**
- * Composer.
+ * Give access to tests_add_filter() function.
+ * 
+ * @link https://github.com/wp-phpunit/example-plugin/blob/master/tests/bootstrap.php
  */
-require_once __DIR__ . '/../vendor/autoload.php';
-
-/**
- * WorDBless.
- */
-\WorDBless\Load::load();
+require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
 /**
  * Psalm.
@@ -26,12 +23,52 @@ if ( defined( 'PSALM_VERSION' ) ) {
 }
 
 /**
+ * SQLite integration.
+ * 
+ * @link https://github.com/WordPress/sqlite-database-integration/issues/7#issuecomment-1646660980
+ * @link https://github.com/wp-phpunit/example-plugin/blob/master/tests/bootstrap.php
+ * @link https://github.com/WordPress/wordpress-playground/blob/23c0fc6aae5d090a14d352160c34d39988167406/packages/playground/wordpress/build/Dockerfile#L25-L42
+ */
+if ( ! is_dir( __DIR__ . '/../wordpress/wp-content/' ) ) {
+	// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.directory_mkdir
+	mkdir( __DIR__ . '/../wordpress/wp-content/' );
+}
+
+$db_dropin_file = __DIR__ . '/../wordpress/wp-content/db.php';
+
+if ( ! is_file( $db_dropin_file ) ) {
+	// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.file_ops_file_put_contents
+	file_put_contents(
+		$db_dropin_file,
+		str_replace(
+			[
+				'{SQLITE_IMPLEMENTATION_FOLDER_PATH}',
+				'{SQLITE_PLUGIN}',
+			],
+			[
+				__DIR__ . '/../vendor/wordpress/sqlite-database-integration',
+				'sqlite-database-integration/load.php',
+			],
+			file_get_contents( __DIR__ . '/../vendor/wordpress/sqlite-database-integration/db.copy' )
+		)
+	);
+}
+
+/**
  * Plugin.
  */
-$pronamic_pay_plugin = \Pronamic\WordPress\Pay\Plugin::instance(
-	array(
-		'action_scheduler' => __DIR__ . '/../vendor/woocommerce/action-scheduler/action-scheduler.php',
-	)
+tests_add_filter(
+	'muplugins_loaded',
+	function () {
+		require __DIR__ . '/../vendor/wordpress/sqlite-database-integration/load.php';
+
+		require __DIR__ . '/../pronamic-pay-core.php';
+	} 
 );
 
-$pronamic_pay_plugin->plugins_loaded();
+/**
+ * Start up the WP testing environment.
+ * 
+ * @link https://github.com/wp-phpunit/example-plugin/blob/master/tests/bootstrap.php
+ */
+require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
