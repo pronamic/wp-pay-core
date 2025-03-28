ae5c5845b910cc49e15fd5af7e960faf23b01e8b
Remco Tolsma <remcotolsma@pronamic.nl>
2025-03-28 11:49:52 +0100

    Update rector.php

--- rector.php
+++ rector.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Rector
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2024 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay
+ */
 
 declare(strict_types=1);
 
