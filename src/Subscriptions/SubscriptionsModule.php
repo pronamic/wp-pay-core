f5e5b21fc86800e1bcf0426486516aac7900a4d3
Remco Tolsma <info@remcotolsma.nl>
2025-08-22 09:46:54 +0200

    Update SubscriptionsModule.php

--- src/Subscriptions/SubscriptionsModule.php
+++ src/Subscriptions/SubscriptionsModule.php
@@ -274,7 +274,7 @@ class SubscriptionsModule {
 		switch ( $_GET['action'] ) {
 			// phpcs:enable WordPress.Security.NonceVerification.Recommended
 			case 'cancel':
-				return $this->handle_subscription_cancel( $subscription );
+				$this->handle_subscription_cancel( $subscription );
 			case 'renew':
 				$this->handle_subscription_renew( $subscription );
 
